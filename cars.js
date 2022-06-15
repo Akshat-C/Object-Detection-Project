190c29dcb0c992afe00864e8e503df8e11e9f036
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-06-15 17:16:52 +1000

    Add files via upload

--- cars.js
+++ cars.js
@@ -1,3 +1,4 @@
+status1 = "";
 function preload()
 {
     img = loadImage("Cars.jpg");
@@ -7,6 +8,27 @@ function setup()
 {
     canvas = createCanvas(550, 500);
     canvas.center();
+
+    o_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Objects Detecting";
+}
+
+function modelLoaded()
+{
+    console.log("Object Detector has been loaded");
+    status1 = true;
+    o_detector.detect(img, gotResults);
+}
+
+function gotResults(error, results)
+{
+    if (error)
+    {
+        console.error()
+    } else 
+    {
+        console.log(results);
+    }
 }
 
 function draw()
