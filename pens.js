b81118755cded7d602c426a435a5b386d67301f6
Akshat-C <85225437+Akshat-C@users.noreply.github.com>
2022-06-22 19:28:35 +1000

    Add files via upload

--- pens.js
+++ pens.js
@@ -1,4 +1,11 @@
 status1 = "";
+object = [];
+recx = 0;
+recy = 0;
+recw = 0;
+rech = 0;
+recn = 0;
+recp = 0;
 function preload()
 {
     img = loadImage("Pens.jpg");
@@ -28,10 +35,30 @@ function gotResults(error, results)
     } else 
     {
         console.log(results);
+        object = results;
     }
 }
 
 function draw()
 {
     image(img, 0, 0, 550, 500);
+    if (status1 != "")
+    {
+        for (i = 0; i < object.length; i++)
+        {
+        recx = object[i].x;
+        recy = object[i].y;
+        recw = object[i].width;
+        rech = object[i].height;
+        recn = object[i].label;
+        recp = object[i].confidence; 
+        document.getElementById("status").innerHTML = "Objects Detetced";
+        fill("red");
+        percentage = floor(recp*100)
+        text(recn +" "+percentage+"%", recx + 15, recy + 15);
+        noFill();
+        stroke("red");
+        rect(recx-120, recy, recw, rech);
+        }
+    }
 }
